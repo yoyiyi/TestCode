752ba8a9c6a76fed2544e0881e6e9e2b39332c5d
zzq <2455676683@qq.com>
2020-08-26 11:56:30 +0800

    yoyiyi

--- app/src/main/java/com/yoyiyi/test/d19/CircleView.kt
+++ app/src/main/java/com/yoyiyi/test/d19/CircleView.kt
@@ -2,8 +2,10 @@ package com.yoyiyi.test.d19
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.yoyiyi.ext.dp
 
 /**
  * 类名：SquareImageView
@@ -13,13 +15,20 @@ import android.view.View
  * @version 1.0
  */
 class CircleView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private val PADDING = 100.dp
+    private val RADIUS = 100.dp
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-
-
+        var size = (((PADDING + RADIUS)) * 2).toInt()
+        val width = resolveSize(size, widthMeasureSpec)
+        val height = resolveSize(size, heightMeasureSpec)
+        setMeasuredDimension(width, height)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        canvas.drawCircle(PADDING + RADIUS, PADDING + RADIUS, RADIUS, paint)
     }
 }
