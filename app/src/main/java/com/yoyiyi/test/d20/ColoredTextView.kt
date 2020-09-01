143c74b857abcaba66eff2af0835f0e1433f2257
zzq <2455676683@qq.com>
2020-09-01 12:06:15 +0800

    yoyiyi

--- app/src/main/java/com/yoyiyi/test/d20/ColoredTextView.kt
+++ app/src/main/java/com/yoyiyi/test/d20/ColoredTextView.kt
@@ -10,34 +10,36 @@ import androidx.appcompat.widget.AppCompatTextView
 import com.yoyiyi.ext.dp
 import java.util.*
 
-private val COLORS = intArrayOf(
-  Color.parseColor("#E91E63"),
-  Color.parseColor("#673AB7"),
-  Color.parseColor("#3F51B5"),
-  Color.parseColor("#2196F3"),
-  Color.parseColor("#009688"),
-  Color.parseColor("#FF9800"),
-  Color.parseColor("#FF5722"),
-  Color.parseColor("#795548")
-)
-private val TEXT_SIZES = intArrayOf(10, 14, 18)
-private val CORNER_RADIUS = 4.dp
-private val X_PADDING = 16.dp.toInt()
-private val Y_PADDING = 8.dp.toInt()
 
 class ColoredTextView(context: Context, attrs: AttributeSet?) : AppCompatTextView(context, attrs) {
-  private var paint = Paint(Paint.ANTI_ALIAS_FLAG)
-  private val random = Random()
+    private val COLORS = intArrayOf(
+        Color.parseColor("#E91E63"),
+        Color.parseColor("#673AB7"),
+        Color.parseColor("#3F51B5"),
+        Color.parseColor("#2196F3"),
+        Color.parseColor("#009688"),
+        Color.parseColor("#FF9800"),
+        Color.parseColor("#FF5722"),
+        Color.parseColor("#795548")
+    )
+    private val TEXT_SIZES = intArrayOf(10, 14, 18)
+    private val CORNER_RADIUS = 4.dp
+    private val X_PADDING = 16.dp.toInt()
+    private val Y_PADDING = 8.dp.toInt()
 
-  init {
-    setTextColor(Color.WHITE)
-    textSize = TEXT_SIZES[random.nextInt(3)].toFloat()
-    paint.color = COLORS[random.nextInt(COLORS.size)]
-    setPadding(X_PADDING, Y_PADDING, X_PADDING, Y_PADDING)
-  }
 
-  override fun onDraw(canvas: Canvas) {
-    canvas.drawRoundRect(0f, 0f, width.toFloat(), height.toFloat(), CORNER_RADIUS, CORNER_RADIUS, paint)
-    super.onDraw(canvas)
-  }
+    private var paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val random = Random()
+
+    init {
+        setTextColor(Color.WHITE)
+        textSize = TEXT_SIZES[random.nextInt(3)].toFloat()
+        paint.color = COLORS[random.nextInt(COLORS.size)]
+        setPadding(X_PADDING, Y_PADDING, X_PADDING, Y_PADDING)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawRoundRect(0f, 0f, width.toFloat(), height.toFloat(), CORNER_RADIUS, CORNER_RADIUS, paint)
+        super.onDraw(canvas)
+    }
 }
